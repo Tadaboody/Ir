efd894a0481fcc4413e2f1bf5d028d827623badd
Tadaboody <tomerpet@gmail.com>
2018-06-18 22:39:33 +0300

    Fix NN model returns
    Before we returned the tf.stack(placeholder) as the placeholder

--- product_team/biLSTMNet.py
+++ product_team/biLSTMNet.py
@@ -10,28 +10,27 @@ def model(word2vec_size):
     timesteps = None  # Answers are in a dynamic length
     num_hidden = 128
     pooling_size = 3
-    answer_placeholder = rnn_placeholder(timesteps, word2vec_size)
-    question_placeholder = rnn_placeholder(timesteps, word2vec_size)
-    rand_answer_placeholder = rnn_placeholder(timesteps, word2vec_size)
     strides = [1]
     M = 2
-
     def lstm(num_hidden):
         return rnn.BasicLSTMCell(num_units=num_hidden, activation=tf.tanh)
-
     lstm_fw_cell = lstm(num_hidden)
     lstm_bw_cell = lstm(num_hidden)
 
-    def bidirectional_pooled_rnn(placeholder):
+    def pass_through_model():
+        placeholder = tf.placeholder(
+            "float", shape=[None, timesteps, word2vec_size])
+        placeholder_through_time = tf.unstack(placeholder, axis=1)
         outputs, _, _ = rnn.stack_bidirectional_dynamic_rnn(
-            cells_fw=lstm_fw_cell, cells_bw=lstm_bw_cell, inputs=placeholder, dtype=tf.float32)
+            cells_fw=lstm_fw_cell, cells_bw=lstm_bw_cell, inputs=placeholder_through_time,
+            dtype=tf.float32)
         outputs = tf.layers.average_pooling1d(
             answer_outputs, pool_size=pooling_size, strides=strides)
-        return outputs
-    answer_outputs = bidirectional_pooled_rnn(answer_placeholder)
-    question_outputs = bidirectional_pooled_rnn(question_placeholder)
-    rand_answer_outputs = bidirectional_pooled_rnn(rand_answer_placeholder)
+        return placeholder, outputs
 
+    answer_placeholder, answer_outputs = pass_through_model()
+    question_placeholder, question_outputs = pass_through_model()
+    rand_answer_placeholder, rand_answer_outputs = pass_through_model()
     loss = model_loss(M, answer_outputs, question_outputs, rand_answer_outputs)
     return answer_placeholder, question_placeholder, rand_answer_placeholder, answer_outputs, loss
 
@@ -54,6 +53,7 @@ def rnn_placeholder(timesteps, word2vec_size):
 def generate_batch(batch_size):
     yield None*3
 
+
 if __name__ == '__main__':
     word2vec_size = 100
     EPOCH_AMOUNT = 20
@@ -64,6 +64,6 @@ if __name__ == '__main__':
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         for epoch_i in range(EPOCH_AMOUNT):
-            for answer_batch,question_batch,random_answer_batch in generate_batch(BATCH_SIZE):
+            for answer_batch, question_batch, random_answer_batch in generate_batch(BATCH_SIZE):
                 sess.run(optimizer, feed_dict={
                          answer_placeholder: answer_batch, question_placeholder: question_batch, rand_answer_placeholder: random_answer_batch})
