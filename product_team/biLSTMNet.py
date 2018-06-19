Fix loss errors by normalizing input

product_team/biLSTMNet.py
@@ -1,12 +1,18 @@
+from os.path import join
+
+import numpy as np
 import tensorflow as tf
 from tensorflow.contrib import rnn
 
-from product_team import WORD2VEC_SIZE, load_index
+from product_team import SAVE_DIR, WORD2VEC_SIZE, load_index
 
 index = load_index()
 
 
-def cosine_similarity(x, y, axis=0, **kwargs):
+def cosine_similarity(x, y, axis=-1, **kwargs):
+    x = tf.nn.l2_normalize(x, axis=axis)
+    y = tf.nn.l2_normalize(y, axis=axis)
+
     return tf.subtract(tf.constant(1.0), tf.losses.cosine_distance(x, y, axis, **kwargs))
 
 
@@ -20,34 +26,41 @@ def model(word2vec_size, max_sentence_length):
 
     def lstm(layer_size, num_layers):
         return [rnn.BasicLSTMCell(num_units=layer_size, activation=tf.tanh) for _ in range(num_layers)]
-    lstm_fw_cell = lstm(layer_size, num_hidden_layers)
-    lstm_bw_cell = lstm(layer_size, num_hidden_layers)
 
     def pass_through_model():
         placeholder = tf.placeholder(
             "float", shape=[None, timesteps, word2vec_size])
-        # placeholder_through_time = tf.unstack(
-        #     placeholder, num=timesteps, axis=1)
+        placeholder_through_time = tf.unstack(
+            placeholder, num=timesteps, axis=1)
         placeholder_through_time = placeholder
-        outputs, _, _ = rnn.stack_bidirectional_dynamic_rnn(
-            cells_fw=lstm_fw_cell, cells_bw=lstm_bw_cell, inputs=placeholder_through_time,
-            dtype=tf.float32)
-        outputs = tf.layers.dense(outputs, layer_size)
-        outputs = tf.layers.average_pooling1d(
-            outputs, pool_size=pooling_size, strides=strides)
-        return placeholder, outputs
+        with tf.variable_scope('RNN', reuse=tf.AUTO_REUSE):
+            lstm_fw_cell = lstm(layer_size, num_hidden_layers)
+            lstm_bw_cell = lstm(layer_size, num_hidden_layers)
+            outputs, _, _ = rnn.stack_bidirectional_dynamic_rnn(
+                cells_fw=lstm_fw_cell, cells_bw=lstm_bw_cell, inputs=placeholder_through_time,
+                dtype=tf.float32)
+            # outputs = tf.layers.conv1d(placeholder_through_time, filters=32, kernel_size=3)
+            outputs = tf.layers.dense(outputs, layer_size)
+            outputs = tf.layers.average_pooling1d(
+                outputs, pool_size=pooling_size, strides=strides)
+            return placeholder, outputs
 
     answer_placeholder, answer_outputs = pass_through_model()
     question_placeholder, question_outputs = pass_through_model()
     rand_answer_placeholder, rand_answer_outputs = pass_through_model()
     loss = model_loss(M, answer_outputs, question_outputs, rand_answer_outputs)
-    return answer_placeholder, question_placeholder, rand_answer_placeholder, answer_outputs, loss
+    return answer_placeholder, question_placeholder, rand_answer_placeholder, question_outputs, loss
+
+
+# def sf_cosine_similarity(a, b, axis=-1):
+#     normalize_a = tf.nn.l2_normalize(a, axis=axis)
+#     normalize_b = tf.nn.l2_normalize(b, axis=axis)
+#     return tf.reduce_sum(tf.multiply(normalize_a, normalize_b))
 
 
 def model_loss(M, answer_outputs, question_outputs, rand_answer_outputs):
     loss = tf.subtract(tf.constant(M), cosine_similarity(
         answer_outputs, question_outputs))
-    print(answer_outputs, question_outputs)
     loss = tf.add(loss, cosine_similarity(
         question_outputs, rand_answer_outputs))
     loss = tf.maximum(0.0, loss)
@@ -64,20 +77,35 @@ def generate_batch(batch_size):
     return index.generate_batch(batch_size)
 
 
-def train():
+MODEL_PATH = join(SAVE_DIR,'bilstmnet.ckpt')
+def train(restore = False):
     word2vec_size = WORD2VEC_SIZE
     EPOCH_AMOUNT = 20
-    BATCH_SIZE = 300
+    BATCH_SIZE = index.test_size//80
     max_sentence_length = index.normalize_vector_length
-    answer_placeholder, question_placeholder, rand_answer_placeholder, answer_outputs, loss_op = model(
+    answer_placeholder, question_placeholder, rand_answer_placeholder, question_outputs, loss_op = model(
         word2vec_size, max_sentence_length)
     optimizer = tf.train.AdamOptimizer(
         learning_rate=0.001).minimize(loss_op)
+    saver = tf.train.Saver()
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+        if restore:
+            saver.restore(sess, MODEL_PATH)
         for epoch_i in range(EPOCH_AMOUNT):
-            for answer_batch, question_batch, random_answer_batch in generate_batch(BATCH_SIZE):
+            for batch_num, (answer_batch, question_batch, random_answer_batch) in enumerate(generate_batch(BATCH_SIZE)):
                 _, loss_val = sess.run([optimizer, loss_op], feed_dict={
                     answer_placeholder: answer_batch, question_placeholder: question_batch, rand_answer_placeholder: random_answer_batch})
-                print(f"batch! loss={loss_val}")
+                print(f"batch #{batch_num} loss={loss_val}")
             print(f"epoch {epoch_i} done")
+            saver.save(sess, MODEL_PATH)
+
+def run(vector :np.ndarray):
+    word2vec_size = WORD2VEC_SIZE
+    max_sentence_length = index.normalize_vector_length
+    answer_placeholder, question_placeholder, rand_answer_placeholder, question_outputs, loss_op = model(
+        word2vec_size, max_sentence_length)
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver.restore(sess,MODEL_PATH)
+        return sess.run(question_outputs, feed_dict={question_placeholder: [vector]})[0]

product_team/index.py
@@ -17,6 +17,8 @@ from statistics import median_high, mean
 from math import floor
 from product_team import EnglishAnalyzer
 from product_team.utils import memorize
+from os.path import join
+from product_team import SAVE_DIR
 
 WORD2VEC_SIZE = 100
 
@@ -48,6 +50,13 @@ class Document:
             yield self.tokenized_best_answer
         for answer in self.tokenized_nbest_answers:
             yield answer
+    
+    # @property
+    # def answer_pairs(self) -> Tuple(str,List[str]):
+    #     if self.tokenized_best_answer:
+    #         yield self.best_answer, self.tokenized_best_answer
+    #     for answer in self.nbest_answers:
+    #         tokenized_answer = global_analay.tokenize(answer)
 
 
 class Index:
@@ -66,7 +75,7 @@ class Index:
     def max_sentence_length(self):
         return max(self.sentance_length_array)
 
-    def __init__(self, dataset_path="dataset/nfL6.json", Word2Vec_path="word2vec", doclist_path="doclist.p"):
+    def __init__(self, dataset_path=join("dataset", "nfL6.json"), Word2Vec_path=join(SAVE_DIR, "word2vec"), doclist_path=join(SAVE_DIR, "doclist.p")):
         self.analyzer = EnglishAnalyzer()
         try:
             with open(doclist_path, 'rb') as fp:
@@ -90,7 +99,7 @@ class Index:
                 Index.Corpus_iterator(self), size=WORD2VEC_SIZE, min_count=1)
             self.model.save(Word2Vec_path)
         self.doc_train, self.doc_test = train_test_split(
-            self.doclist, test_size=0.2)
+            self.doclist, test_size=0.1)
 
     @lazy
     def normalize_vector_length(self):
@@ -105,6 +114,10 @@ class Index:
     
     def average_sentance_length(self):
         return floor(mean(self.sentance_length_array))
+    
+    @lazy
+    def test_size(self):
+        return sum(len(list(doc.tokenized_answers)) for doc in self.doc_test)
 
 
     def process_for_train(self, text: str):
@@ -127,12 +140,9 @@ class Index:
                     yield self.process_for_train(answer), self.process_for_train(doc.tokenized_question), self.process_for_train(random_answer())
 
         generator = doc_iterator()
-        while True:
+        for _ in range(self.test_size//batch_size):
             ret = list(zip(*islice(generator, batch_size)))
-            if ret:
-                yield np.array(ret)
-            else:
-                return
+            yield np.array(ret)
 
     def search(self, query: str):
         ...

tests/test_index.py
@@ -32,23 +32,27 @@ def question():
 #     return ret
 
 
-def test_word2vec_vocab(index: Index):
-    for sentence in index.Corpus_iterator(index):
-        assert index.model[sentence].shape[1] == 100
-
-
 def test_batches(index: Index):
-    batch_size = 30
-    for _, batch_triple in zip(range(3), index.generate_batch(batch_size)):
+    batch_size = index.test_size//30
+    for i, batch_triple in enumerate(index.generate_batch(batch_size)):
         assert len(batch_triple) == 3
         assert all(len(batch) <= batch_size for batch in batch_triple)
         assert all([len(batch.shape) == 3 for batch in batch_triple])
         assert all(batch.dtype == np.float for batch in batch_triple)
 
+def test_word2vec_vocab(index: Index):
+    for sentence in index.Corpus_iterator(index):
+        assert index.model[sentence].shape[1] == 100
 
-def test_max_sentence_len(index: Index):
-    assert index.max_sentence_length > 0
-    print(index.max_sentence_length)
+
+
+def test_batches_end(index :Index):
+    num_batches = 30
+    batch_size = len(index.doclist)//num_batches
+    gen = iter(index.generate_batch(batch_size))
+    with pytest.raises(StopIteration):
+        for _ in range(num_batches*2):
+            _ = next(gen)
 
 
 def test_search(index: Index, question):